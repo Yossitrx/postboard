e195054467a2e991dafd8285596dd74ac7c3f665
yossi <yossitrx@gmail.com>
2019-06-13 11:48:40 +0300

    Post board

--- src/app/common/consts/note.const.ts
+++ src/app/common/consts/note.const.ts
@@ -2,31 +2,31 @@ import { Note } from './note.interface';
 
 export const NOTES: Set<Note> = new Set <Note> ([
   {
-    authorName: 'WILLIAM',
+    authorName: 'William',
     content: 'Lorem ipsum dolor sit amet',
     date: new Date(2019, 5, 1).toLocaleDateString('en-US'),
     id: 1,
   },
   {
-    authorName: 'JAMES',
+    authorName: 'James',
     content: 'Lorem ipsum dolor sit amet',
     date: new Date(2019, 5, 2).toLocaleDateString('en-US'),
     id: 2,
   },
   {
-    authorName: 'DANIEL',
+    authorName: 'Daniel',
     content: 'Lorem ipsum dolor sit amet',
     date: new Date(2019, 5, 3).toLocaleDateString('en-US'),
     id: 3,
   },
   {
-    authorName: 'a',
+    authorName: 'Lior',
     content: 'Lorem ipsum dolor sit amet',
     date: new Date(2019, 5, 4).toLocaleDateString('en-US'),
     id: 4,
   },
   {
-    authorName: 'a',
+    authorName: 'Rami',
     content: 'Lorem ipsum dolor sit amet, consectetur ' +
       'adipiscing elit, sed do eiusmod tempor ' +
       'incididunt ut labore et dolore magna ' +
@@ -37,19 +37,19 @@ export const NOTES: Set<Note> = new Set <Note> ([
     id: 5,
   },
   {
-    authorName: 'SAMUEL',
+    authorName: 'Samuel',
     content: 'Lorem ipsum dolor sit amet',
     date: new Date(2019, 5, 6).toLocaleDateString('en-US'),
     id: 6,
   },
   {
-    authorName: 'YOSSI',
+    authorName: 'Yossi',
     content: 'Lorem ipsum dolor sit amet',
     date: new Date(2019, 5, 7).toLocaleDateString('en-US'),
     id: 7,
   },
   {
-    authorName: 'DON',
+    authorName: 'Don',
     content: 'Lorem ipsum dolor sit amet',
     date: new Date(2019, 5, 8).toLocaleDateString('en-US'),
     id: 8,
